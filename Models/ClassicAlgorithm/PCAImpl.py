7cc82670f37f8334446e5266e4a4055bd4f386c6
Meng Chen <lakechen@outlook.com>
2017-10-19 20:55:47 -0500

    change PCAImpl inheritance

--- Models/ClassicAlgorithm/PCAImpl.py
+++ Models/ClassicAlgorithm/PCAImpl.py
@@ -3,25 +3,21 @@
 from sklearn.decomposition import PCA
 from Preprocess import Preprocess
 
-class PCAImpl:
+class PCAImpl(PCA):
 
     def __init__(self, density=0.8, n_components=None, copy=True, whiten=False,\
                                         svd_solver='auto', tol=0.0, \
                                         iterated_power='auto', random_state=None):
-        self.pca = PCA(n_components=n_components, copy=copy, whiten=whiten, \
+                                        
+        PCA.__init__(self, n_components=n_components, copy=copy, whiten=whiten, \
                                         svd_solver=svd_solver, tol=tol, \
                                         iterated_power=iterated_power, \
                                         random_state=random_state)
         self.preprocess = Preprocess(data='fundamental_ratios', density=density)
         self.numPC = 0
-
-
-    def fit(self, data):
-        return self.pca.fit(data)
-        
-
+    
+    
     def getComponents(self, retention=None):
         if retention is not None:
-            varRatio = self.pca.explained_variance_ratio_
-            self.numPC = sum(i > (1.0-retention) for i in varRatio)
-        return self.pca.components_[:self.numPC]
+            self.numPC = sum(i > (1.0-retention) for i in self.explained_variance_ratio_)
+        return self.components_[:self.numPC]
