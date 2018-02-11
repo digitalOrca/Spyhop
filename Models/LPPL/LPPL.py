09a1e2347470cda6ac5738f727c34c40189f3767
Meng Chen <lakechen@outlook.com>
2018-02-10 21:08:08 -0600

    minor extension to LPPL

--- Models/LPPL/LPPL.py
+++ Models/LPPL/LPPL.py
@@ -4,18 +4,19 @@ import random
 import numpy as np
 
 #reference: https://arxiv.org/pdf/1003.2920.pdf
-def LogPeriodicPowerLaw(series, A, B, Tc, m, C, omega, phi):
-    deltaT = np.power(np.subtract(Tc, series), m)
-    oscillatory_term = np.multiply(np.multiply(deltaT, np.cos(np.add(np.multiply(np.log(deltaT), omega), phi))), C)
+def LogPeriodicPowerLaw(series, A, B, Tc, beta, C, omega, phi):
+    deltaT = np.power(np.subtract(Tc, series), beta)
     exponential_term = np.multiply(deltaT, B)
+    oscillatory_term = np.multiply(np.multiply(deltaT, np.cos(np.add(np.multiply(np.log(deltaT), omega), phi))), C)
     fitted = A + exponential_term + oscillatory_term
     residual = np.subtract(series, fitted)
     return residual
 
 def randomParameters():
-    # B>0
-    # 0 < m < 1
-    # T > n
-    m = random.uniform(0, 1)
+    # reference: https://arxiv.org/pdf/1002.1010.pdf
+    beta = np.random.uniform(low=0.15, high=0.51, size=1)[0]
+    omega = np.random.uniform(low=4.8, high=7.96, size=1)[0]
+    phi = np.random.uniform(low=0, high=2*np.pi, size=1)[0]
+
 
 
