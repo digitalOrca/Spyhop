3265032e7a5d7c988114361ca371bfab46156089
Meng Chen <lakechen@outlook.com>
2018-02-11 21:17:28 -0600

    minor update to LPPL

--- Models/LPPL/LPPL.py
+++ Models/LPPL/LPPL.py
@@ -9,11 +9,13 @@ def LogPeriodicPowerLaw(series, A, B, Tc, beta, C, omega, phi):
     exponential_term = np.multiply(deltaT, B)
     oscillatory_term = np.multiply(np.multiply(deltaT, np.cos(np.add(np.multiply(np.log(deltaT), omega), phi))), C)
     fitted = A + exponential_term + oscillatory_term
-    residual = np.subtract(series, fitted)
+    residual = np.subtract(series, fitted)  # use raw price, not log price (10)
     return residual
 
 def randomParameters():
     # reference: https://arxiv.org/pdf/1002.1010.pdf
+    # B < 0
+    # T is the number of trading days in the bubble
     beta = np.random.uniform(low=0.15, high=0.51, size=1)[0]
     omega = np.random.uniform(low=4.8, high=7.96, size=1)[0]
     phi = np.random.uniform(low=0, high=2*np.pi, size=1)[0]
